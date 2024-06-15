67a2fd56b505ebaf71b8c4ba996961f5c2e772b7
ComputerCow <48889860+StinkyMilo@users.noreply.github.com>
2024-06-15 10:53:17 -0400

    added shapes and more poetry

--- LetterFrequency/find_words_for_aliases.py
+++ LetterFrequency/find_words_for_aliases.py
@@ -2,7 +2,7 @@ import trigger_detection as td
 import json
 aliases = ["int","lit","add","ide","sub","log","exp","mod","tim","lis","ind","app","rep","del",
            "whe", "les", "mor", "als", "oth", "par", "inv", "whi", "fre" "fun", "pri", "was", 
-           "sta", "not"]
+           "sta", "not", "rec", "lin", "arc"]
 
 words = [i.split(',')[0] for i in open('unigram_freq.csv','r').read().split('\n')][1:]
 
