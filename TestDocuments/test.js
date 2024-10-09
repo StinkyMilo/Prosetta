94336757ecaacef4ef12c8f6bbf127d363deff19
ComputerCow <48889860+StinkyMilo@users.noreply.github.com>
2024-10-08 21:06:16 -0400

    why isn't it working?

--- TestDocuments/test.js
+++ TestDocuments/test.js
@@ -1,21 +1,15 @@
-function new_var(state_var, digital_var, unity_var) {
-    let machines_var = 90;
-    let without_var = 0;
-    move_to(digital_var, unity_var);
-    while ((without_var < 7)) {
-        if ((without_var < 4)) {
-            rotate_delta(machines_var);
-        }
-        else {
-            rotate_delta(-machines_var);
-        }
-        if ((years_var[state_var][without_var] > 4)) {
-            draw_line(freedom_var);
-        }
-        else {
-            move_to(freedom_var);
-        }
-        without_var = (without_var + 1);
-    }
-    rotate_delta(-machines_var);
+function all_var(num_var, x_var, y_var) {
+    let temp_var = (num_var / 10);
+    let num1_var = (temp_var - (temp_var % 1));
+    let num2_var = (num_var % 10);
+    drawsingledigitnum_var(num1_var, x_var, y_var)
+    drawsingledigitnum_var(num2_var, (x_var + segdist_var + padding_var), y_var)
+}
+
+function drawnum_var(num_var, x_var, y_var) {
+    let temp_var = (num_var / 10);
+    let num1_var = (temp_var - (temp_var % 1));
+    let num2_var = (num_var % 10);
+    drawsingledigitnum_var(num1_var, x_var, y_var)
+    drawsingledigitnum_var(num2_var, (x_var + segdist_var + padding_var), y_var)
 }
