172649f940f85b6a113b10054ff54a183258b9a7
ComputerCow <48889860+StinkyMilo@users.noreply.github.com>
2024-10-26 18:08:42 -0400

    added wasm bindings

---
+++ docs/wasm-bindings/prosetta_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_parserrunner_free(a: number, b: number): void;
+export function __wbg_parserrunnerdata_free(a: number, b: number): void;
+export function parserrunner_new(): number;
+export function parserrunner_run_to_completion(a: number, b: number, c: number): number;
+export function parserrunnerdata_get_javascript(a: number, b: number): void;
+export function parserrunnerdata_get_html(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
