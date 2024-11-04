6fb983561abf77e32247b7839fde63559af6f1b6
Flynn Duniho <flynn@harveyosity.org>
2024-11-04 11:21:00 -0500

    Remove commented out function

--- Rust Interpreter/src/parser.rs
+++ Rust Interpreter/src/parser.rs
@@ -571,13 +571,3 @@ impl<'a> Parser<'a> {
         self.cached_fails = HashMap::new();
     }
 }
-
-// /// https://stackoverflow.com/questions/31101915/how-to-implement-trim-for-vecu8
-// pub fn trim_ascii_whitespace(x: &[u8]) -> &[u8] {
-//     let from = match x.iter().position(|x| !x.is_ascii_whitespace()) {
-//         Some(i) => i,
-//         None => return &x[0..0],
-//     };
-//     let to = x.iter().rposition(|x| !x.is_ascii_whitespace()).unwrap();
-//     &x[from..=to]
-// }
