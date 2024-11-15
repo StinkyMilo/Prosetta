1101a2d5a8db41a5689a32b644c5c7a2307c186a
ComputerCow <48889860+StinkyMilo@users.noreply.github.com>
2024-11-15 17:30:56 -0500

    part of speech analysis start

--- LetterFrequency/PartsOfSpeech/get_parts_of_speech.py
+++ LetterFrequency/PartsOfSpeech/get_parts_of_speech.py
@@ -4,7 +4,7 @@ part_sources = {
     "noun":[
         "nouns/mostly-nouns.txt",
         "nouns/mostly-nouns-ment.txt",
-        "mostly-plural-nouns.txt"
+        "nouns/mostly-plural-nouns.txt"
     ],
     "verb":[
         "verbs/mostly-verbs-infinitive.txt",
@@ -22,8 +22,28 @@ part_sources = {
     ]
 }
 
+def flatten(ls):
+    output = []
+    for ls2 in ls:
+        for item in ls2:
+            output.append(item)
+    return output
+
+word_sets = {i:set(flatten([open("PartOfSpeechList/"+j,"r").read().split("\n") for j in part_sources[i]])) for i in part_sources}
+
+all_words = open("../words_alpha.txt","r").read().split("\n")
+
 """
 Go through words_alpha.txt and cross-reference with part_sources to generate. If it fits in none, add it to other. Otherwise add it to all categories it matches
 Make a JS dictionary that, for each word, gives it a list of parts of speech.
 When making the popup, go through each word and instead of just adding it, add it to the *correct* subsections.
 """
+
+word_dict = {}
+
+for word in all_words:
+    pos = []
+    for part in word_sets:
+        if word in word_sets[part]:
+            pos.append(part)
+    
