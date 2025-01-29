8e39f42962ca5e738a5234b3d7f5f880ea3d09f0
ComputerCow <mjacobs2@wpi.edu>
2025-01-29 16:39:28 -0500

    regenerated tests + sidebar + import poem

--- Frontend/docs/Imports.md
+++ Frontend/docs/Imports.md
@@ -17,9 +17,7 @@ This example imports graphics and frames
 My Poem
 by Milo Jacobs, John Graphton, and Alice Framingham.
 \t
-was frame ide fra 5!
-whi mor frame 20!
-was frame sub frame 20...
+was frame mod ide fra 5. 20!
 tur ide tim frame frame. 2!
 rec tim frame frame!
 `' 
--- Frontend/docs/Imports_exp.js
+++ Frontend/docs/Imports_exp.js
@@ -1,3 +1,3 @@
 `My Poem`;
-"By Milo Jacobs, John Graphton, Alice Framingham, t was frame ide fra 5, whi mor frame 20, was frame sub frame 20, tur ide tim frame frame, 2, rec tim frame frame";
+"By Milo Jacobs, John Graphton, Alice Framingham, t was frame mod ide fra 5, 20, tur ide tim frame frame, 2, rec tim frame frame";
 
